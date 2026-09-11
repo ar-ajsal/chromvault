9dc6fbdd497e860be01570c565cd0ee815df45ed
Abdu rahman ajsal <ajsala321@gmail.com>
2026-09-11 11:07:33 +0530

    Configure vercel.json for Next.js and remove root index.html

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/': ['./public/framer-page.html'],
+    },
+  },
   images: {
     remotePatterns: [
       {
